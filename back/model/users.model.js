4385c9d003486ba95866d27a324a1c8b7d0e05fa
Marcel <ezenagumlgmail.com>
2021-07-06 14:57:19 +0100

    deposit works same as account update route

--- back/model/users.model.js
+++ back/model/users.model.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: {type: String, unique:true},
+    name: {type: String},
     phoneNumber: {type: String, unique:true},
     email: {type: String, unique:true},
     password: {type: String, required: true},
-    accountNumber: {type: Number, unique: true, required: true},
+    accountNumber: {type: String, unique: true, required: true},
     deposit:[{type: mongoose.Schema.Types.ObjectId, ref: "Deposit"}]
 
 })
--- back/routes/userRoute.js
+++ back/routes/userRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const Deposit =  require('../model/savings.model')
-const User =  require('../model/users.model')
+const Deposits =  require('../model/savings.model')
+const Users =  require('../model/users.model')
 const bcrypt =  require('bcryptjs');
 const { listeners } = require('../model/savings.model');
 
@@ -14,13 +14,13 @@ usersRouter.post('/register', async(req, res) => {
    const reqUser = req.body;
     try {
     reqUser.accountNumber = reqUser.phoneNumber.slice(1,)
-        const user = new User(reqUser);
+        const user = new Users(reqUser);
         user.save();
         // console.log( user)
 
         // await Deposit.deleteMany({});
 
-        const deposit = new Deposit({accountNumber:user.accountNumber,
+        const deposit = new Deposits({accountNumber:user.accountNumber,
             amount: 0,
             depositor: req.body.name,
             balance: 0,
@@ -40,8 +40,9 @@ usersRouter.post('/register', async(req, res) => {
 
 
 usersRouter.get('/', async(req, res) => {
+    // await Users.deleteMany({})
 
-    const users = await User.find();
+    const users = await Users.find({});
 
     res.send(users)
 });
@@ -50,20 +51,24 @@ usersRouter.get('/', async(req, res) => {
 usersRouter.patch('/deposit/:id', async(req, res) => {
     const id = req.params.id;
     // get user from db and get account number and balance
-    const accountOwner = await User.findById(id).select("accountNumber")
+    const accountOwner = await Users.findById(id).select("accountNumber")
+    console.log(accountOwner)
     const {amount, depositor, accountNumber} = req.body
+    console.log(req.body)
     try {
         if(accountNumber != accountOwner.accountNumber){
             res.send(`${accountNumber} is not a valid Account number. "`)
         } else {
         //    await Deposit.deleteMany();
         console.log(`here's the accountNumber ${accountNumber}`)
-            Deposit.findOneAndUpdate({accountNumber}, {$set: req.body}&& {$inc: {'balance': + req.body.amount}} , (error, foundAccount) => {
+            Deposits.findOneAndUpdate({accountNumber}, {$set: req.body}&& {$inc: {'balance': + req.body.amount}} , (error, foundAccount) => {
                 if(error){                   
                         console.log(error)
                         // foundAccount.inc('foundAccount.balance': + foundAccount.amount)
                     }
                     console.log(`found account ${foundAccount}`)
+
+                    res.send(`You have credited ${foundAccount.accountNumber} with ${amount}.`)
             })
                               
             }
@@ -78,11 +83,14 @@ usersRouter.patch('/deposit/:id', async(req, res) => {
 // get account details
 usersRouter.get('/:id/account', async(req, res) => {
    
-    const userDetail = await User.findById(req.params.id).select("accountNumber")
- console.log(userDetail._id)
+    const userDetail = await Users.findById(req.params.id).select("accountNumber")
 
  
- const accountDetails = await Deposit.findById({_id: userDetail._id}, (err, accountInfo) => {
+    console.log(typeof userDetail.accountNumber)
+
+
+ 
+ const accountDetails = await Deposits.findOne({accountNumber : userDetail.accountNumber}, "accountNumber balance", (err, accountInfo) => {
 if(err){
     console.log(`There was an error as ${err}`)
 }
